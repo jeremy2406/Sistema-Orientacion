32af238b966a32dc9d8026973e6410507d9cbdaf
EnmanuelAbreu <enmanuelabreu393@gmail.com>
2025-03-30 23:33:17 -0400

    Arreglando

--- Componentes/Nav.php
+++ Componentes/Nav.php
@@ -1,6 +1,22 @@
-<!-- Actualizar las rutas en la navegación -->
-<a href="estudiantes.php" class="nav-link">
-    <i class="fas fa-users"></i>
-    <span>Estudiantes</span>
-</a>
-<!-- ... otros enlaces de navegación ... -->
+<aside class="sidebar">
+    <div class="logo_content">
+        <div class="logo">
+        <a href="./index.php">
+            <img src="./Imagenes/Logo.png" alt="Logo">
+        </a>
+            <div class="logo_name">Orientación</div>
+        </div>
+        <i class="fa-solid fa-bars" id="btn"></i>
+        
+    </div>
+
+    <ul class="nav_list">
+        <li><a href="./Estudiantes.php"><i class="fas fa-handshake"></i> <span class="link_name">Estudiantes</span></a></li>
+        <li><a href="./Excusas.php"><i class="fas fa-user-graduate"></i><span  class="link_name">Excusas</span> </a></li>
+        <li><a href="./Calendario.php"><i class="fas fa-book-open"></i> <span  class="link_name">Calendario</span></a></li>
+        <li><a href="./Tardanzas.php"><i class="fas fa-book"></i> <span  class="link_name">Tardanzas</span></a></li>
+        <li><a href="./Faltas.php"><i class="fas fa-cogs"></i> <span  class="link_name">Faltas</span></a></li>
+        <li><a href="./Reportes.php"><i class="fas fa-chart-bar"></i> <span class="link_name">Reportes</span></a></li>
+    </ul>
+    
+</aside>
