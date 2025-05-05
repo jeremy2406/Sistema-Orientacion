3284bdfcddc5428225546d96ee476a2803abc5ac
EnmanuelAbreu <enmanuelabreu393@gmail.com>
2025-05-05 00:59:16 -0400

    Arreglando direccion

--- Componentes/config.php
+++ Componentes/config.php
@@ -1,4 +1,9 @@
 <?php
-    // config.php
-    define('BASE_URL', '/Sistema-Orientacion/'); // ajusta esto si tu carpeta tiene otro nombre
+    // Detecta si estás en localhost o en un servidor
+    if (strpos($_SERVER['HTTP_HOST'], 'localhost') !== false) {
+        define('BASE_URL', '/Sistema-Orientacion/'); // RUTA EN LOCAL
+    } else {
+        define('BASE_URL', '/'); // RUTA EN PRODUCCIÓN (Render)
+    }
+    
 ?>
