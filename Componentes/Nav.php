40902ccc08f104b5ae6c3c1f99414dd32a34be3d
EnmanuelAbreu <enmanuelabreu393@gmail.com>
2025-05-05 21:38:36 -0400

    Mas cambios, para que todo funcione

--- Componentes/Nav.php
+++ Componentes/Nav.php
@@ -1,5 +1,11 @@
 <?php include 'config.php'; ?><!-- o donde esté BASE_URL -->
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Css/Estilos.css">
 <aside class="sidebar">
     <div class="logo_content">
         <div class="logo">
