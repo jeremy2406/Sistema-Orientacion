9a3008a44c17505332a89b597735a8f2f5272d36
jeremy2406 <diazjeremy344@gmail.com>
2025-05-07 00:15:15 -0400

    nuevo
    Terminada parte de creadores

--- Componentes/Creadores.php
+++ Componentes/Creadores.php
@@ -1,4 +1,25 @@
 <?php include '../Componentes/header.php'; ?>
 <?php include '../Componentes/Nav.php'; ?>
 
+
+  <div class="containers">
+    <h1>Creadores</h1>
+    <div class="cards">
+      <img src="<?= BASE_URL ?>Imagenes/Jeremy.jpg" alt="Imagen 1" class="cards-img">
+      <h2>Jeremy Díaz</h2>
+      <p>Creador de la página web. Estudiante de 6to de Desarrollo y Administración de Aplicaciones Informáticas.</p>
+    </div>
+    <div class="cards">
+      <img src="<?= BASE_URL ?>Imagenes/Enmanuel.jpg" alt="Imagen 2" class="cards-img">
+      <h2>Enmanuel Abreu</h2>
+      <p>Creador de la página web. Estudiante de 6to de Desarrollo y Administración de Aplicaciones Informáticas.</p>
+    </div>
+    <div class="cards">
+      <img src="<?= BASE_URL ?>Imagenes/Marco.png" alt="Imagen 3" class="cards-img">
+      <h2>Marco Santos</h2>
+      <p>Creador de la página web. Estudiante de 6to de Desarrollo y Administración de Aplicaciones Informáticas.</p>
+    </div>
+  </div>
+
+
 <?php include '../Componentes/footer.php'; ?>
