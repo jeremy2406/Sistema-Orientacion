0d0e8bcd1715e09dc0969b5d98a5dbb259a54fc7
EnmanuelAbreu <enmanuelabreu393@gmail.com>
2025-05-07 09:40:57 -0400

    Ultimos cambios

--- Componentes/Creadores.php
+++ Componentes/Creadores.php
@@ -7,17 +7,17 @@
     <div class="cards">
       <img src="<?= BASE_URL ?>Imagenes/Jeremy.jpg" alt="Imagen 1" class="cards-img">
       <h2>Jeremy Díaz</h2>
-      <p>Creador de la página web. Estudiante de 6to de Desarrollo y Administración de Aplicaciones Informáticas.</p>
+      <p><B>Creador Original</B> de la página web. Estudiante de 6to de Desarrollo y Administración de Aplicaciones Informáticas. <B>Promoción(2021-2025)</B></p>
     </div>
     <div class="cards">
       <img src="<?= BASE_URL ?>Imagenes/Enmanuel.jpg" alt="Imagen 2" class="cards-img">
       <h2>Enmanuel Abreu</h2>
-      <p>Creador de la página web. Estudiante de 6to de Desarrollo y Administración de Aplicaciones Informáticas.</p>
+      <p><B>Creador Original</B> de la página web. Estudiante de 6to de Desarrollo y Administración de Aplicaciones Informáticas. <B>Promoción(2021-2025)</B></p>
     </div>
     <div class="cards">
       <img src="<?= BASE_URL ?>Imagenes/Marco.png" alt="Imagen 3" class="cards-img">
       <h2>Marco Santos</h2>
-      <p>Creador de la página web. Estudiante de 6to de Desarrollo y Administración de Aplicaciones Informáticas.</p>
+      <p><B>Creador Original</B> de la página web. Estudiante de 6to de Desarrollo y Administración de Aplicaciones Informáticas. <B>Promoción(2021-2025)</B></p>
     </div>
   </div>
 
